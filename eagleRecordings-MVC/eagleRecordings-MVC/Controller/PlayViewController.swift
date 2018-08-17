da7f4fa9ae602f3ecf8b2050da50c4292af94ef6
yinyongzhen <eagle_01yz@163.com>
2018-08-17 17:57:09 +0800

    player.swift

--- eagleRecordings-MVC/eagleRecordings-MVC/Controller/PlayViewController.swift
+++ eagleRecordings-MVC/eagleRecordings-MVC/Controller/PlayViewController.swift
@@ -5,8 +5,35 @@
 //  Created by yongzhen on 2018/8/15.
 //  Copyright © 2018年 yongzhen. All rights reserved.
 //
-
+import UIKit
 import Foundation
-class PlayViewController: UIViewController {
+class PlayViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var progressLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    @IBOutlet var noRecordingLabel: UILabel!
+    @IBOutlet var activeItemElements: UIView!
+    var audioPlayer: Player?
+    var recording: Recording?{
+        didSet{
+            updateForChangedRecording()
+        }
+    }
     
+    func updateForChangedRecording() {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+fileprivate extension String {
+    static let uuidPathKey = "uuidPath"
+    
+    static let pause = NSLocalizedString("Pause", comment: "")
+    static let resume = NSLocalizedString("Resume playing", comment: "")
+    static let play = NSLocalizedString("Play", comment: "")
 }
---
+++ eagleRecordings-MVC/eagleRecordings-MVC/Helper/Player.swift
@@ -0,0 +1,12 @@
+//
+//  Player.swift
+//  eagleRecordings-MVC
+//
+//  Created by yongzhen on 2018/8/17.
+//  Copyright © 2018年 yongzhen. All rights reserved.
+//
+
+import Foundation
+class Player: NSObject {
+    
+}
